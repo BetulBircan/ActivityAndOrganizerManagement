7e5c442aa46c84bd71d5a9ebd00855f9164038d7
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-05 23:39:12 +0300

    Etkinlikte kuralları belirleyebilmek için validation oluşturuldu

---
+++ ActivityOrganizerManager/OrganizationActivityManager/Validators/ActivityValidator.cs
@@ -0,0 +1,14 @@
+﻿using AcitivityOrganizerManager.ViewModels;
+using FluentValidation;
+using OrganizationActivityManager.ViewModels;
+
+namespace OrganizationActivityManager.Validators
+{
+    public class ActivityValidator : AbstractValidator<ActivityViewModel>
+    {
+        public ActivityValidator()
+        {
+            RuleFor(activity => activity.Quota).Must(quota => quota > 0).WithMessage("Geçerli Kontenjan Bilgisi Giriniz");
+        }
+    }
+}
