da57c27afb640b39bd1df8e376e29f6853c9f081
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-06 00:02:28 +0300

    Login işlemi

--- ActivityOrganizerManager/OrganizationActivityManager/Controllers/SubscriberController.cs
+++ ActivityOrganizerManager/OrganizationActivityManager/Controllers/SubscriberController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ActivitiesDataBase.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OrganizationActivityManager.Controllers
@@ -7,5 +8,32 @@ namespace OrganizationActivityManager.Controllers
     [ApiController]
     public class SubscriberController : ControllerBase
     {
+        ActivitiesContext context = new ActivitiesContext();
+
+        [HttpGet]
+        public IActionResult GetActivities()
+        {
+            Activitiy activitynew = new Activitiy();
+
+
+            var query = from a in context.Activitiys
+                        join c in context.Categories on a.CategoryId equals c.CategoryId
+                        join ci in context.Cities on a.CityId equals ci.CityId
+                        join co in context.Companies on a.CompanyId equals co.CompanyId
+                        select new
+                        {
+                            a.ActivityName,
+                            a.ActivityDate,
+                            a.ApplicationDeadline,
+                            a.Description,
+                            a.Quota,
+                            c.CategoryName,
+                            ci.CityName,
+                            a.Isticked,
+                            a.Company.CompanyName,
+                        };
+
+            return Ok(query);
+        }
     }
 }
---
+++ ActivityOrganizerManager/OrganizationActivityManager/Controllers/UserLoginController.cs
@@ -0,0 +1,36 @@
+﻿using Acitivity.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Activities.Models;
+
+namespace Acitivity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserLoginController : ControllerBase
+    {
+
+        ActivitiesContext context = new ActivitiesContext();
+        
+        [HttpPost]
+        public IActionResult Create(UserLoginViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Subscriber subscriber = new Subscriber();
+            Organizer organizer = new Organizer();
+
+            subscriber.UserEmail = user.UserEmail;
+            subscriber.UserPassword = user.UserPassword;
+
+           
+            return BadRequest();
+
+            
+            
+        }
+    }
+}
