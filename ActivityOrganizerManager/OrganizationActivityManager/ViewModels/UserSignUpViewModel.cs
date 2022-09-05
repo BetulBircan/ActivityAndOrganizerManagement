1b58338e25f4a2cb56975ca5410b977e7f96aa0d
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-05 22:50:51 +0300

    kullanıcı kaydı için view model oluşturuldu

---
+++ ActivityOrganizerManager/OrganizationActivityManager/ViewModels/UserSignUpViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OrganizationActivityManager.ViewModels
+{
+    public class UserSignUpViewModel
+    {
+        public string UserName { get; set; } = null!;
+        public string UserSurname { get; set; } = null!;
+        public string UserEmail { get; set; } = null!;
+        public string UserPassword { get; set; } = null!;
+        public string UserPasswordAgain { get; set; } = null!;
+
+        public int RoleId { get; set; }
+    }
+}
