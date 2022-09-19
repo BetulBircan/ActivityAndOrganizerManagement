dad315e66de54f577cfcf9743d2dac688632aeb0
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-20 02:36:28 +0300

    kategoriye göre etkinlik listeleme yapıldı

--- ActivityOrganizerManager/OrganizationActivityManager/Controllers/SubscriberController.cs
+++ ActivityOrganizerManager/OrganizationActivityManager/Controllers/SubscriberController.cs
@@ -1,39 +1,42 @@
-﻿using ActivitiesDataBase.Models;
+﻿using AcitivityOrganizerManager.ViewModels;
+using ActivitiesDataBase.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OrganizationActivityManager.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class SubscriberController : ControllerBase
     {
         ActivitiesContext context = new ActivitiesContext();
 
-        [HttpGet]
-        public IActionResult GetActivities()
+        [HttpGet("{categoryId}")]
+        public IActionResult GetActivitiesByCategory(int categoryId)
         {
-            Activitiy activitynew = new Activitiy();
+           
+
+            var query = context.Activitiys.Where(x => x.CategoryId == categoryId).ToList();
 
 
-            var query = from a in context.Activitiys
-                        join c in context.Categories on a.CategoryId equals c.CategoryId
-                        join ci in context.Cities on a.CityId equals ci.CityId
-                        join co in context.Companies on a.CompanyId equals co.CompanyId
-                        select new
-                        {
-                            a.ActivityName,
-                            a.ActivityDate,
-                            a.ApplicationDeadline,
-                            a.Description,
-                            a.Quota,
-                            c.CategoryName,
-                            ci.CityName,
-                            a.Isticked,
-                            a.Company.CompanyName,
-                        };
+            //var query = from a in context.Activitiys
+            //            join c in context.Categories on a.CategoryId equals c.CategoryId
+            //            join ci in context.Cities on a.CityId equals ci.CityId
+            //            join co in context.Companies on a.CompanyId equals co.CompanyId
+            //            select new
+            //            {
+            //                a.ActivityName,
+            //                a.ActivityDate,
+            //                a.ApplicationDeadline,
+            //                a.Description,
+            //                a.Quota,
+            //                c.CategoryName,
+            //                ci.CityName,
+            //                a.Isticked,
+            //                a.Company.CompanyName,
+            //            };
 
             return Ok(query);
         }
