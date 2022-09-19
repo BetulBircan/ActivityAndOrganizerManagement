c73eb8728465923cf6ca8f1888067def87398fc8
BetulBircan <bircan.betul_1996@hotmail.com>
2022-09-20 02:55:50 +0300

    kategoriye, şehire göre ve de tüm etkinlikler listelendi

--- ActivityOrganizerManager/OrganizationActivityManager/Controllers/SubscriberController.cs
+++ ActivityOrganizerManager/OrganizationActivityManager/Controllers/SubscriberController.cs
@@ -13,12 +13,12 @@ namespace OrganizationActivityManager.Controllers
     {
         ActivitiesContext context = new ActivitiesContext();
 
-        [HttpGet("{categoryId}")]
-        public IActionResult GetActivitiesByCategory(int categoryId)
+        [HttpGet]
+        public IActionResult GetActivities()
         {
-           
 
-            var query = context.Activitiys.Where(x => x.CategoryId == categoryId).ToList();
+
+            var query = context.Activitiys.ToList();
 
 
             //var query = from a in context.Activitiys
@@ -40,5 +40,65 @@ namespace OrganizationActivityManager.Controllers
 
             return Ok(query);
         }
+
+        [HttpGet("{categoryId}")]
+        public IActionResult GetActivitiesByCategory(byte categoryId)
+        {
+           
+
+            var category = context.Activitiys.Where(x => x.CategoryId == categoryId).ToList();
+
+
+            //var query = from a in context.Activitiys
+            //            join c in context.Categories on a.CategoryId equals c.CategoryId
+            //            join ci in context.Cities on a.CityId equals ci.CityId
+            //            join co in context.Companies on a.CompanyId equals co.CompanyId
+            //            select new
+            //            {
+            //                a.ActivityName,
+            //                a.ActivityDate,
+            //                a.ApplicationDeadline,
+            //                a.Description,
+            //                a.Quota,
+            //                c.CategoryName,
+            //                ci.CityName,
+            //                a.Isticked,
+            //                a.Company.CompanyName,
+            //            };
+
+            return Ok(category);
+        }
+
+        
+        [HttpGet("~/{cityId}")]
+        
+        public IActionResult GetActivitiesByCity(int cityId)
+        {
+
+
+            var query = context.Activitiys.Where(x => x.CityId == cityId).ToList();
+
+
+            //var query = from a in context.Activitiys
+            //            join c in context.Categories on a.CategoryId equals c.CategoryId
+            //            join ci in context.Cities on a.CityId equals ci.CityId
+            //            join co in context.Companies on a.CompanyId equals co.CompanyId
+            //            select new
+            //            {
+            //                a.ActivityName,
+            //                a.ActivityDate,
+            //                a.ApplicationDeadline,
+            //                a.Description,
+            //                a.Quota,
+            //                c.CategoryName,
+            //                ci.CityName,
+            //                a.Isticked,
+            //                a.Company.CompanyName,
+            //            };
+
+            return Ok(query);
+        }
+
+       
     }
 }
